ccc9206f78d2132e62d5b62cada1288a3ee5935f
AimiLeia <papagiannaki.aimilia@gmail.com>
2026-07-12 08:42:19 +0300

    rerun fix again

--- tmr_app.py
+++ tmr_app.py
@@ -164,13 +164,13 @@ with tab1:
         if st.button("⬅️ Previous", use_container_width=True):
             if st.session_state.day > df["day"].min():
                 st.session_state.day -= 1
-                st.experimental_rerun ()
+                st.rerun ()
 
     with col_next:
         if st.button("Next ➡️", use_container_width=True):
             if st.session_state.day < df["day"].max():
                 st.session_state.day += 1
-                st.experimental_rerun ()
+                st.rerun ()
     st.markdown("""
     <script>
     window.scrollTo(0, 0);
